97bd31fc1f6f0d3fa3b5c78d8239c9d5c63b3d7c
VNS Rohit K <rohitkanteti@gmail.com>
2020-11-04 19:51:58 +0530

    first code added

---
+++ Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingAssignment3
+{
+    // IMPORTANT: Only add code in the section
+    // indicated below. The code I've provided
+    // makes your solution work with the 
+    // automated grader on Coursera
+
+    /// <summary>
+    /// Programming Assignment 3
+    /// </summary>
+    class Program
+    {
+        // number to classify
+        static int number;
+
+        /// <summary>
+        /// Classifies numbers as even or odd and negative, 0, or positive
+        /// </summary>
+        /// <param name="args">command-line args</param>
+        static void Main(string[] args)
+        {
+            // loop while there's more input
+            string input = Console.ReadLine();
+            while (input[0] != 'q')
+            {
+                // extract number from string
+                GetInputValueFromString(input);
+
+                // Add your code between this comment
+                // and the comment below. You can of
+                // course add more space between the
+                // comments as needed
+                
+                int oddEvenflag = 0;
+                
+                if(number%2 == 0){
+                   oddEvenflag = 1;
+                }
+                else{ oddEvenflag = 0;}
+                switch(oddEvenflag)
+                    {
+                       case 1:
+                        if(number == 0){
+                        
+                            Console.WriteLine("e"+" "+"0");
+                        
+                        }
+                        else if(number>0){
+                           
+                             Console.WriteLine("e"+" "+"1");
+                        
+                        }
+                        else{
+                        
+                         Console.WriteLine("e"+" "+"-1");
+                        }
+
+                        break;
+
+                        case 0:
+                        if(number>0){
+                           
+                             Console.WriteLine("o"+" "+"1");
+                        
+                        }
+                        else{
+                        
+                         Console.WriteLine("o"+" "+"-1");
+                        }
+                        break;
+                
+                
+                    }
+
+
+ 
+
+                // Don't add or modify any code below
+                // this comment
+                input = Console.ReadLine();
+            }
+        }
+
+        /// <summary>
+        /// Extracts the number from the given input string
+        /// </summary>
+        /// <param name="input">input string</param>
+        static void GetInputValueFromString(string input)
+        {
+            number = int.Parse(input);
+        }
+    }
+}
